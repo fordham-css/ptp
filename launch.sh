dbdedced9122dd65d9bb4cb6a311f020639ec77f
Aaron DeVera <adevera@fordham.edu>
2016-04-13 09:00:00 -0400

    pages init

--- launch.sh
+++
@@ -1,6 +0,0 @@
-./redis-1.2.5/redis-server ./redis-1.2.5/redis.conf
-#python vis-api-server.py $1
-python manager.py $1
-pkill 'redis-server'
-rm *.pyc
-rm *.sh~
--- manual-shutoff.sh
+++
@@ -1,4 +0,0 @@
-pkill 'python'
-pkill 'redis-server'
-rm *.pyc
-rm *.sh~
